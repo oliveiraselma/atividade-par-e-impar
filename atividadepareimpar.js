63ceaa1993f80bb77c05ae11c9b5f7eeca0a0f96
selma aluna <oliveira.selma@gmail.com>
2022-07-10 10:13:58 -0300

    par e impar

---
+++ atividadepareimpar.js
@@ -0,0 +1,16 @@
+let alunosPresentes = 17;
+
+for (let aluno=0; aluno <= alunosPresentes; aluno++)  {
+    
+    if (aluno==0){
+        console.log("zero" + aluno);
+    }
+
+        if (aluno %2 ==0) {// par
+        console.log("par" + aluno);
+    } else {
+         console.log("impar" + aluno);
+    }
+}
+
+         
